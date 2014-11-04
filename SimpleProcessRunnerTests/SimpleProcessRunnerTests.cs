bf5f9e18a5392073aade4de8b61fc6f3b65c7b4e
Jeff Ashton <jeff.ashton@d2l.com>
2014-11-03 22:18:14 -0500

    Adding some basic tests

---
+++ SimpleProcessRunnerTests/SimpleProcessRunnerTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using SimpleProcessRunner;
+
+namespace SimpleProcessRunnerTests {
+
+	[TestFixture]
+	[Category( "Integration" )]
+	public sealed class SimpleProcessRunnerTests {
+
+		private ISimpleProcessRunner m_runner;
+
+		[TestFixtureSetUp]
+		public void TestFixtureSetUp() {
+			m_runner = new SimpleProcessRunner.SimpleProcessRunner();
+		}
+
+		[Test]
+		public void Timeout() {
+
+			Assert.Throws<TimeoutException>(
+				() => {
+					m_runner.Run( "git.exe", "clone http://test.org", TimeSpan.FromSeconds( 1 ) );
+				}
+			);
+		}
+
+		[Test]
+		public void StandardOutput() {
+
+			ProcessResult result = m_runner.Run(
+					@"C:\Windows\System32\cmd.exe",
+					"/C echo hello world",
+					TimeSpan.FromSeconds( 10 )
+				);
+
+			Assert.AreEqual( 0, result.ExitCode );
+			Assert.AreEqual( "hello world", result.StandardOutput.Trim() );
+			Assert.IsEmpty( result.StandardError );
+		}
+
+		[Test]
+		public void StandardError() {
+
+			ProcessResult result = m_runner.Run(
+					@"C:\Windows\System32\cmd.exe",
+					@"/C dir Boom:\ReallyNotFound",
+					TimeSpan.FromSeconds( 10 )
+				);
+
+			Assert.AreEqual( 1, result.ExitCode );
+			Assert.IsEmpty( result.StandardOutput );
+			Assert.AreEqual( "The filename, directory name, or volume label syntax is incorrect.", result.StandardError.Trim() );
+		}
+	}
+}
