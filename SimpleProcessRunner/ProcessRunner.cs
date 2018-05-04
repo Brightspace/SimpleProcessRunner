382e3291fc3b3c6b26e4c0affeaee8e5394bb17b
Jeff Ashton <Jeff.Ashton@D2L.com>
2018-05-04 11:40:24 -0400

    Calling cancel on output streams

--- SimpleProcessRunner/ProcessRunner.cs
+++ SimpleProcessRunner/ProcessRunner.cs
@@ -106,6 +106,9 @@ namespace SimpleProcessRunner {
 								arguments
 							);
 
+						p.CancelOutputRead();
+						p.CancelErrorRead();
+
 						string standardOutputTxt;
 						lock( standardOutput ) {
 							standardOutputTxt = standardOutput.ToString();
